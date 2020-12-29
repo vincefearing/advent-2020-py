f92d8a3f82a603b21b8a891e19d68376ef85c8c6
Vincent Fearing <vincefearing@gmail.com>
2020-12-29 13:00:22 -0800

    Day 2 part 2 complete

--- day2/source/main.py
+++ day2/source/main.py
@@ -1,8 +1,10 @@
+
 def main():
     with open("/mnt/e/advent-2020-py/day2/data/data.txt") as inFile:
         expenseReport = []
         expenseReport = inFile.read().splitlines()
         part1(expenseReport)
+        part2(expenseReport)
         
    
 def part1(expenseReport):
@@ -15,6 +17,18 @@ def part1(expenseReport):
         count = value.count(key)
         if count >= int(numRange[0]) and count <= int(numRange[1]):
             valid += 1
+    print(valid)
+
+def part2(expenseReport):
+    valid = 0
+    for i in expenseReport:
+        temp = i.split(" ")
+        numRange = temp[0].split("-")
+        key = temp[1][0]
+        value = temp[2]
+        if (value[int(numRange[0]) - 1] == key) ^ (value[int(numRange[1]) - 1] == key):
+            valid += 1
+        
     print(valid)
 
 if __name__ == "__main__":
